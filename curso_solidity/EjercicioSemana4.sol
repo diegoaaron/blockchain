7a703274de13997f0e51b779a0363b6e17d7c9ef
Diego Aarón <diegoaarondv@gmail.com>
2023-06-25 21:35:27 -0500

    solidity 1 - 15

--- curso_solidity/EjercicioSemana4.sol
+++ curso_solidity/EjercicioSemana4.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.18;
 
 /**
@@ -29,45 +30,62 @@ pragma solidity 0.8.18;
 
 contract EjercicioSemana4 {
     // definir address 'admin' con valor de 0x08Fb288FcC281969A0BBE6773857F99360f2Ca06
-    // address public admin = 0x08Fb288FcC281969A0BBE6773857F99360f2Ca06;
+    address public admin = 0x08Fb288FcC281969A0BBE6773857F99360f2Ca06;
     // definir boolean public 'pausado' con valor de false
-    // bool public pausado;
+    bool public pausado;
 
     // 7 - definición de modifier 'soloAdmin'
     // verificar que el msg.sender sea el admin
     // poner el comodín fusión al final del método
-    // modifier soloAdmin() {
-    //     // ...logica
-    // }
+    modifier soloAdmin() {
+        require(admin == msg.sender, "Solo el admin puede ejecutar esta funcion");
+        _;
+    }
 
     // 8 - definir modifier 'cuandoPausado'
     // modifier cuandoNoPausado
     // verificar que el boolean 'pausado' sea false
+    modifier cuandoNoPausado() {
+        require(pausado == false, "El contrato esta pausado");
+        _;
+    }
 
     // 1 - definición de variables locales
     // definir variable que almacena el balance total del cajero automático (e.g. balanceTotal)
     // definir mapping que almacena el balance de cada usuario (e.g. balances)
-    // balanceTotal;
-    // mapping()balances;
+    uint256 public balanceTotal;
+    mapping(address add_usuario => uint user_balance) public balances;
 
     // 2 - definición de eventos importantes
     // definir eventos 'Deposit', 'Transfer' y 'Withdraw'
     // - Deposit tiene dos parámetros: address y uint256 que son (from, value)
+    event Deposit (address from, uint256 value);
+
     // - Transfer tiene tres parámetros: address, address y uint256 que son (from, to, value)
+    event Transfer (address from, address to, uint256 value);
+
     // - Withdraw tiene dos parámetros: address y uint256 que son (to, value)
-    // event Deposit (...);
-    // event Transfer (...);
-    // event Withdraw (...);
+    event Withdraw (address to, uint256 value);
 
     // 5 - definición de error personalizado 'SaldoInsuficiente'
-    // SaldoInsuficiente
+    modifier SaldoInsuficiente2(uint256 _cantidad) {
+        require(_cantidad < balances[msg.sender], "Saldo insuficiente");
+        _;
+    }
+
+    error SaldoInsuficiente();
+
 
     // 3 - definición de método 'depositar'
     // definir función 'depositar' que recibe un parámetro uint256 '_cantidad' y es 'public'
     // - aumentar el balance del usuario en '_cantidad'
     // - aumentar el balance total del cajero automático en '_cantidad'
     // - emitir evento 'Deposit'
-    function depositar(uint256 _cantidad) public {}
+    function depositar(uint256 _cantidad) public {
+        balances[msg.sender] += _cantidad;
+        balanceTotal += _cantidad;
+        emit Deposit(msg.sender, _cantidad);
+    }
 
     // 4 - definición de método 'retirar'
     // definir función 'retirar' que recibe un parámetro uint256 '_cantidad' y es 'public'
@@ -75,7 +93,15 @@ contract EjercicioSemana4 {
     // - disminuir el balance del usuario en '_cantidad'
     // - disminuir el balance total del cajero automático en '_cantidad'
     // - emitir evento 'Withdraw'
-    function retirar(uint256 _cantidad) public {}
+    function retirar(uint256 _cantidad) public cuandoNoPausado {
+        if (_cantidad >= balances[msg.sender]) {
+            revert ("Saldo insuficiente");
+        } else {
+            balances[msg.sender] -= _cantidad;
+            balanceTotal -= _cantidad;
+            emit Withdraw(msg.sender, _cantidad);
+        }
+    }
 
     // 6 - definición de método 'transferir'
     // definir función 'transferir' que recibe dos parámetros: address '_destinatario' y uint256 '_cantidad' y es 'public'
@@ -83,13 +109,21 @@ contract EjercicioSemana4 {
     // - disminuir el balance del usuario en '_cantidad'
     // - aumentar el balance del destinatario en '_cantidad'
     // - emitir evento 'Transfer'
-    function transferir(address _destinatario, uint256 _cantidad) public {}
+    function transferir(address _destinatario, uint256 _cantidad) public {
+        if (_cantidad >= balances[msg.sender]) {
+            revert SaldoInsuficiente();
+        } else {
+            balances[msg.sender] -= _cantidad;
+            balances[_destinatario] += _cantidad;
+            emit Transfer(msg.sender, _destinatario, _cantidad);
+        }
+    }
 
     // 8 - definición de método 'cambiarPausado'
     // definir función 'cambiarPausado' que es 'public' y solo puede ser llamada por el admin (usar modifier)
     // - cambiar el boolean 'pausado' a su valor contrario
     // - emitir
-    // function cambiarPausado() public soloAdmin {
-    //     pausado = !pausado;
-    // }
+    function cambiarPausado() public soloAdmin {
+        pausado = !pausado;
+    }
 }
