3e477acece0991bf0eb253c6973cd6caeaa7d066
diego aaron <diegoaarondv@gmail.com>
2024-01-13 22:36:16 -0500

    update cripto 3/n

--- blockchain_bites/semana_1/MiPrimerContrato2.sol
+++ blockchain_bites/semana_1/MiPrimerContrato2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 // El nombre del archivo no tiene que coincidir con el nombre del contrato
 // ES buena práctica hacer que coincida (legibilidad)
